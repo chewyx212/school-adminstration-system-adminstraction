7a1a482066615018b0b7834ae9970c7c74be17cb
chewyx212 <chewingyx212@gmail.com>
2026-05-10 17:47:26 +0800

    CSV util update

--- typescript/src/utils/index.ts
+++ typescript/src/utils/index.ts
@@ -1,14 +1,24 @@
 import fs from 'fs';
 import csv from 'csv-parser';
-import { CsvItem } from 'CsvItem';
+import { CsvItem, ParsedCsv } from 'CsvItem';
 
-export const convertCsvToJson = (filePath: string): Promise<CsvItem[]> => {
+export const parseCsv = (filePath: string): Promise<ParsedCsv<CsvItem>> => {
   const results: CsvItem[] = [];
+  let headers: string[] = [];
   const stream = fs.createReadStream(filePath).pipe(csv());
 
   return new Promise((resolve, reject) => {
+    stream.on('headers', (csvHeaders: string[]) => {
+      headers = csvHeaders;
+    });
     stream.on('data', (data: CsvItem) => results.push(data));
-    stream.on('end', () => resolve(results));
+    stream.on('end', () => resolve({ headers, rows: results }));
     stream.on('error', (err) => reject(err));
   });
+}
+
+export const convertCsvToJson = async (filePath: string): Promise<CsvItem[]> => {
+  const parsed = await parseCsv(filePath);
+
+  return parsed.rows;
 }
